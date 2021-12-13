5e1ddf635fdd33373874ab61b5c806cbf1c087d6
rahmathidayatmajid <rahmat@pajanlom.com>
2021-12-13 22:30:06 +0700

    + user signup, signup

---
+++ database/migrations/20211213131642-create-employe.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('Employes', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            employe_name: {
+                type: Sequelize.STRING(100)
+            },
+            employe_role: {
+                type: Sequelize.ENUM('engineer', 'hrd', 'analis')
+            },
+            employe_phone_number: {
+                type: Sequelize.STRING(13)
+            },
+            employe_address: {
+                type: Sequelize.STRING(225)
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async(queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Employes');
+    }
+};
